486315d28bc66e28028156abf35670732b851f01
Shek Rasel <your-email@example.com>
2025-04-12 14:36:26 +0600

    ....

--- data/question.js
+++ data/question.js
@@ -11,57 +11,57 @@ export const questionsData = [
     options: ["Lion", "Cow", "Tiger", "Cat"],
     ans: "Tiger",
   },
-  // {
-  //   id: 3,
-  //   question: "What is the capital of France?",
-  //   options: ["Paris", "London", "Berlin", "Madrid"],
-  //   ans: "Paris"
-  // },
-  // {
-  //   id: 4,
-  //   question: "Who invented JavaScript?",
-  //   options: ["Brendan Eich", "Tim Berners-Lee", "Elon Musk", "Mark Zuckerberg"],
-  //   ans: "Brendan Eich"
-  // },
-  // {
-  //   id: 5,
-  //   question: "Which planet is known as the Red Planet?",
-  //   options: ["Earth", "Mars", "Jupiter", "Venus"],
-  //   ans: "Mars"
-  // },
-  // {
-  //   id: 6,
-  //   question: "Which language is used to style web pages?",
-  //   options: ["HTML", "JQuery", "CSS", "XML"],
-  //   ans: "CSS"
-  // },
-  // {
-  //   id: 7,
-  //   question: "What does HTTP stand for?",
-  //   options: [
-  //     "HyperText Transfer Protocol",
-  //     "HighText Transfer Protocol",
-  //     "HyperText Transmission Protocol",
-  //     "Hyper Tool Transfer Protocol"
-  //   ],
-  //   ans: "HyperText Transfer Protocol"
-  // },
-  // {
-  //   id: 8,
-  //   question: "Which organ purifies our blood?",
-  //   options: ["Lungs", "Heart", "Kidney", "Liver"],
-  //   ans: "Kidney"
-  // },
-  // {
-  //   id: 9,
-  //   question: "Which company developed the React library?",
-  //   options: ["Google", "Facebook", "Microsoft", "Apple"],
-  //   ans: "Facebook"
-  // },
-  // {
-  //   id: 10,
-  //   question: "What is the boiling point of water?",
-  //   options: ["90°C", "100°C", "110°C", "120°C"],
-  //   ans: "100°C"
-  // }
+  {
+    id: 3,
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    ans: "Paris"
+  },
+  {
+    id: 4,
+    question: "Who invented JavaScript?",
+    options: ["Brendan Eich", "Tim Berners-Lee", "Elon Musk", "Mark Zuckerberg"],
+    ans: "Brendan Eich"
+  },
+  {
+    id: 5,
+    question: "Which planet is known as the Red Planet?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    ans: "Mars"
+  },
+  {
+    id: 6,
+    question: "Which language is used to style web pages?",
+    options: ["HTML", "JQuery", "CSS", "XML"],
+    ans: "CSS"
+  },
+  {
+    id: 7,
+    question: "What does HTTP stand for?",
+    options: [
+      "HyperText Transfer Protocol",
+      "HighText Transfer Protocol",
+      "HyperText Transmission Protocol",
+      "Hyper Tool Transfer Protocol"
+    ],
+    ans: "HyperText Transfer Protocol"
+  },
+  {
+    id: 8,
+    question: "Which organ purifies our blood?",
+    options: ["Lungs", "Heart", "Kidney", "Liver"],
+    ans: "Kidney"
+  },
+  {
+    id: 9,
+    question: "Which company developed the React library?",
+    options: ["Google", "Facebook", "Microsoft", "Apple"],
+    ans: "Facebook"
+  },
+  {
+    id: 10,
+    question: "What is the boiling point of water?",
+    options: ["90°C", "100°C", "110°C", "120°C"],
+    ans: "100°C"
+  }
 ];
